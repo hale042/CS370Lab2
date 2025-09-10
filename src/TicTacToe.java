c5f78ddd3b55f2f31092e91ebe3d16743cd6a9c8
Luke Hale <luahale@ucsc.edu>
2025-09-10 14:27:24 -0700

    initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,27 @@
+public class TicTacToe {
+    private final int BOARDSIZE = 3;
+    private enum Status {WIN, DRAW, CONTINUE}
+    private char[][] board;
+    private boolean firstPlayer;
+    private boolean gameOver;
+
+    public void play(){
+
+    }
+    private void printStatus(int player){
+
+    }
+    private Status gameStatus(){
+
+    }
+    public void printBoard(){
+
+    }
+    private void printSymbol(int column, char value){
+
+    }
+    private boolean validMove(int row, int column){
+
+    }
+
+}
